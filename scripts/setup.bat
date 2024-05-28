edb68ecdae66abcae52638d865cdd8c4a563bf90
_Robertas <66907345+RobertasJ@users.noreply.github.com>
2024-05-29 00:02:38 +0200

    Fix paths to instancesync

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -2,7 +2,7 @@
 
 type NUL > .git/hooks/post-merge
 echo #!/bin/sh > .git/hooks/post-merge
-echo java -jar InstanceSync.jar mod_list.json mods >> .git/hooks/post-merge
+echo java -jar scripts/InstanceSync.jar mod_list.json mods >> .git/hooks/post-merge
 
 echo Done setting up hooks
 echo Running InstanceSync
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,5 @@
 echo "#!/bin/sh" > .git/hooks/post-merge
-echo "java -jar InstanceSync.jar mod_list.json mods" >> .git/hooks/post-merge
+echo "java -jar scripts/InstanceSync.jar mod_list.json mods" >> .git/hooks/post-merge
 
 echo "Done setting up hooks"
 echo "Running InstanceSync"
